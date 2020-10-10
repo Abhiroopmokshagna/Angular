1d757b283a9a5a461081a0d7909b1a7d86a8fafd
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-10-10 12:03:47 +0530

    single page application part 1

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
+import { AboutusComponent } from '../aboutus/aboutus.component';
+import { homedir } from 'os';
+
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,23 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+
+
 import { AppComponent } from './app.component';
+
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -23,20 +32,25 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
