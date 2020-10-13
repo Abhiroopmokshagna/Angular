710b6dabe7ea60cf97bc34399928ede5c86768bf
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-10-13 11:02:41 +0530

    dialogs

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -5,12 +5,12 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 import { AboutusComponent } from '../aboutus/aboutus.component';
-import { homedir } from 'os';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
     {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutusComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
